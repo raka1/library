83870daf47473f662c13cfe086e45b4f6bce6f3f
Tukang Sate <p.raka54@ymail.com>
2019-11-25 22:32:36 +0700

    Pertamax.

---
+++ index.js
@@ -0,0 +1,7 @@
+// hotspot pada digital library repository
+exports.AbstractLoader = undefined;
+exports.DocLoader = undefined;
+exports.FilterController = undefined;
+exports.DocServeController = undefined;
+exports.RequestDataService = require('./lib/RequestDataService');
+exports.Search = undefined;
