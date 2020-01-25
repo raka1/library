ac8afd20c717b0dff3f24b6097b6e6d33ad57dcc
Tukang Sate <p.raka54@ymail.com>
2020-01-25 13:42:00 +0700

    Single search selesai.

--- template/Search.js
+++ template/Search.js
@@ -13,7 +13,16 @@ class Search extends relibrary.Callback {
   }
 
   onError(error) {
-    return error;
+    switch(error) {
+      case 'ERR01':
+        return 'Diperlukan argumen pada saat memanggil hook method.';
+        break;
+      case 'ERR02':
+        return 'Argumen untuk memanggil hook method harus bertipe object.';
+        break;
+      case 'ERR03':
+        return 'Argumen objek setidaknya memiliki property method, action, dan attribute.';
+        break;
   }
 }
 
