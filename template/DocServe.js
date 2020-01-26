5d3e51710b49c0a343c07feb3edd312ca127a9ea
Tukang Sate <p.raka54@ymail.com>
2020-01-26 19:20:37 +0700

    Penetapan beberapa DocServe untuk method get.

--- template/DocServe.js
+++ template/DocServe.js
@@ -13,7 +13,20 @@ class DocServe extends relibrary.Callback {
   }
 
   onError(error) {
-    return error;
+    switch(error) {
+      case 'ERR01':
+        return 'Diperlukan argumen pada saat memanggil hook method.';
+        break;
+      case 'ERR02':
+        return 'Argumen untuk memanggil hook method harus bertipe object.';
+        break;
+      case 'ERR03':
+        return 'Argumen objek setidaknya memiliki property sql atau request.';
+        break;
+      case 'ERR04':
+        return '';
+        break;
+    }
   }
 }
 
